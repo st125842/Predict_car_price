96c572e2a7e33b0a4674e474447b2a5584a8f7a2
Peerapat Ngamgsanga <st125842@ait.asia>
2025-10-03 01:46:44 +0700

    CICD UPDATE

--- Code/test_model_stage.py
+++ Code/test_model_stage.py
@@ -11,29 +11,10 @@ sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 from utils import *
 from models.model import Polynomial,LinearRegression,LogisticRegression
 
-def test_model_output_shape():
-    # model
-    # mf_model = mlflow_model()
-    year = 2020
-    max_power =  92.15565
-    engine =  1248.0
-    seats =  5.0
-    fuel =  0
-    transmission = 1
-    # scaler = load_scaler(scaler_path)
-    scaler_path = get_scaler_path()
-    model_path,_ = get_model_path()
 
-    scaler = load_scaler(scaler_path)
-    model = joblib.load(model_path)
-    # model_path,model_url = load_model_predict
-    X = [[year, engine, max_power, seats, fuel, transmission]]
-    X_scaled = scaler.transform(X)
-
-    pred = model.predict(X_scaled)
-    # print(os.getprint(os.getcwd)
-    # print(pred.shape)
-    assert pred.shape == (1,), f"Expected prediction shape (1,), got {pred.shape}"
+def test_load_model():
+    model = mlflow_model()
+    assert model
 
 def test_stage_model_output_shape():
     # model
