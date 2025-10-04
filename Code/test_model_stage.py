f2911c7121bfa6c25cd1bb369c7d3fa1c12d302e
Peerapat Ngamsanga <6410685223@cn102>
2025-10-04 16:34:46 +0700

    CICD

--- Code/test_model_stage.py
+++ Code/test_model_stage.py
@@ -5,18 +5,28 @@ from dash._utils import AttributeDict
 import os
 import joblib
 import sys
+from dotenv import load_dotenv
 
 # print(os.getcwd())
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 from utils import *
+from models.model import LogisticRegression
 # from models.model import Polynomial,LinearRegression,LogisticRegression
+dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+load_dotenv(dotenv_path)
 
+@pytest.fixture(scope="module")
+def a3_model():
+    user = os.environ['MLFLOW_USERNAME']
+    pas = os.environ['MLFLOW_PASSWORD']
+    mlflow.set_tracking_uri(f"https://{user}:{pas}@mlflow.ml.brain.cs.ait.ac.th")
+    return mlflow.sklearn.load_model(model_uri="models:/st125842-a3-model/1")
 
-def test_load_model():
-    model = mlflow_model()
-    assert model
-
-def test_stage_model_output_shape():
+def test_a3_model_load(a3_model):
+    assert a3_model is not None
+    # assert a3_scaler is not None
+    
+def test_stage_model_output_shape(a3_model):
     # model
     # mf_model = mlflow_model()
     year = 2020
@@ -30,12 +40,12 @@ def test_stage_model_output_shape():
     # model_path,_ = get_model_path()
 
     scaler = load_scaler(scaler_path)
-    model = joblib.load("models/Staging/st125842-a3-model")
+    # model = a3_model()
     # model_path,model_url = load_model_predict
     X = [[year, engine, max_power, seats, fuel, transmission]]
     X_scaled = scaler.transform(X)
 
-    pred = model.predict(X_scaled)
+    pred = a3_model.predict(X_scaled)
     # print(os.getprint(os.getcwd)
     # print(pred.shape)
     assert pred.shape == (1,), f"Expected prediction shape (1,), got {pred.shape}"
---
+++ Code/transition.py
@@ -0,0 +1,37 @@
+def register_model_to_production():
+    import os
+    import warnings
+    import urllib3
+    from mlflow import MlflowClient
+
+    warnings.filterwarnings("ignore", category=urllib3.exceptions.InsecureRequestWarning)
+
+    # Set MLflow authentication
+    
+    # os.environ['MLFLOW_TRACKING_USERNAME'] = 'admin'
+    # os.environ['MLFLOW_TRACKING_PASSWORD'] = 'password'
+    # os.environ['MLFLOW_TRACKING_URI'] = "https://mlflow.ml.brain.cs.ait.ac.th"
+
+    model_name = os.environ['APP_MODEL_NAME']
+
+    client = MlflowClient()
+    print(f"Looking for model: {model_name}")
+
+    try:
+        registered_model = client.get_registered_model(model_name)
+    except Exception as e:
+        raise RuntimeError(f"Model '{model_name}' not found in MLflow registry: {e}")
+
+    for mv in registered_model.latest_versions:  # type: ignore
+        if mv.current_stage == "Staging":
+            version = mv.version
+            client.transition_model_version_stage(
+                name=model_name,
+                version=version,
+                stage="Production",
+                archive_existing_versions=True
+            )
+            print(f"Model version {version} promoted to Production")
+            break
+    else:
+        print("No model found in Staging stage to promote.")
